ea6074c6afe48bb47cd808a68ca5f0740cc55117
Hanchuan Peng <pengh@pengh-lm4.local>
2010-06-03 20:39:33 -0400

    add chanel assemble function in v3d_interface.h

--- Source/V3DInterface/v3d_interface.h
+++ Source/V3DInterface/v3d_interface.h
@@ -6,8 +6,9 @@
 * The License Information and User Agreement should be seen at http://penglab.janelia.org/proj/v3d .
 * 
 * 2009-Aug-21
-* Last edit: 2010-06-01
-* Last edit: 2010-06-02: add setPluginOutputAndDisplayUsingGlobalSetting()
+* 2010-06-01
+* 2010-06-02: add setPluginOutputAndDisplayUsingGlobalSetting()
+* 2010-06-03: add two others function to extract and assemble the results of different channels 
 ********************************************************************************************************
 */
 
@@ -83,7 +84,7 @@ public:
 			const V3DPluginArgList & input, V3DPluginArgList & output, QWidget * parent) = 0;
 };
 
-
+//a function for considering the global setting to extract the IDs of the channels for plugin processing
 inline QList<V3DLONG> getChannelListForProcessingFromGlobalSetting( V3DLONG nc, V3DPluginCallback & callback ) //nc is the # of channels in an image
 {
 	QList<V3DLONG> chlist;
@@ -114,7 +115,7 @@ inline QList<V3DLONG> getChannelListForProcessingFromGlobalSetting( V3DLONG nc, 
 	return chlist;	
 }
 
-
+//a function for considering the global setting to extract the DATA of the channels for plugin processing
 inline QList<V3D_Image3DBasic> getChannelDataForProcessingFromGlobalSetting( Image4DSimple * p, V3DPluginCallback & callback)
 {
 	QList<V3D_Image3DBasic> dlist;
@@ -135,6 +136,8 @@ inline QList<V3D_Image3DBasic> getChannelDataForProcessingFromGlobalSetting( Ima
 	return dlist;
 }
 
+
+//a function for considering the global setting for data conversion
 template <class T> bool setPluginOutputAndDisplayUsingGlobalSetting(T * pluginoutputimg1d, V3DLONG sz0, V3DLONG sz1, V3DLONG sz2, V3DLONG sz3, V3DPluginCallback & callback)
 {
 	if (!pluginoutputimg1d || sz0<=0 || sz1<=0 || sz2<=0 || sz3<=0 )
@@ -227,6 +230,64 @@ template <class T> bool setPluginOutputAndDisplayUsingGlobalSetting(T * pluginou
 	
 	return true;
 }
+
+//a function for assembling output of filtered channels of images
+inline bool assembleProcessedChannels2Image4DClass(QList<V3D_Image3DBasic> & pd, V3DPluginCallback & cb)
+{
+	V3DLONG i;
+	for (i=0; i<pd.size(); i++)
+	{
+		if (!pd[i].data1d || pd[i].sz0<=0 || pd[i].sz1<=0 || pd[i].sz2<=0 || (pd[i].datatype!=V3D_UINT8 && pd[i].datatype!=V3D_UINT16 && pd[i].datatype!=V3D_FLOAT32) )
+		{
+			v3d_msg(QString("The %1 channel of the input to assembleProcessedChannels2Image4DClass() is invalid. Don't output the plugin results.\n").arg(i));
+			return false;
+		}
+		if (i>=1)
+		{
+			if (pd[i].sz0!=pd[i-1].sz0 || pd[i].sz1!=pd[i-1].sz1 || pd[i].sz2!=pd[i-1].sz2 || pd[i].datatype!=pd[i-1].datatype)
+			{
+				v3d_msg(QString("The %1 and %2 channels of the input have different sizes in assembleProcessedChannels2Image4DClass() is invalid. Don't output the plugin results.\n").arg(i).arg(i-1));
+				return false;
+			}
+		}
+	}
+	V3DLONG mysz0 = pd[0].sz0, mysz1 = pd[0].sz1, mysz2 = pd[0].sz2, mysz3 = pd.size();
+	ImagePixelType curdatatype = pd[0].datatype;
+	V3DLONG nunitbytes = 1; if (curdatatype==V3D_UINT16) nunitbytes=2; else if (curdatatype==V3D_FLOAT32) nunitbytes=4; 
+	V3DLONG nchanbytes = mysz0*mysz1*mysz2*nunitbytes;
+	unsigned char *pout = 0;
+	
+	try {
+		pout = new unsigned char [nchanbytes * mysz3];
+	}
+	catch (...) 
+	{
+		v3d_msg(QString("Fail to allocate a buffer memory for output of the plugin in assembleProcessedChannels2Image4DClass()."));
+		return false;
+	}
+	
+	//now copy data
+	for (i=0;i<mysz3;i++)
+	{
+		unsigned char *psrc = pd[i].data1d;
+		unsigned char *psrc_end = pd[i].data1d + nchanbytes;
+		unsigned char *pdst = pout + nchanbytes * i;
+		while (psrc < psrc_end)
+			*(++pdst) = *(++psrc);
+	}
+	
+	//now set V3D display
+	switch (curdatatype)
+	{
+		case V3D_UINT8:   return setPluginOutputAndDisplayUsingGlobalSetting(pout, mysz0, mysz1, mysz2, mysz3, cb); break;
+		case V3D_UINT16:  return setPluginOutputAndDisplayUsingGlobalSetting((unsigned short int *)pout, mysz0, mysz1, mysz2, mysz3, cb); break;
+		case V3D_FLOAT32: return setPluginOutputAndDisplayUsingGlobalSetting((float *)pout, mysz0, mysz1, mysz2, mysz3, cb); break;
+		default: return false;
+	}
+	
+	return false;
+}
+
 
 // obsolete interface for manipulating only one image at a time. 
 class V3DSingleImageInterface
