ad526d760549c277a292c8b6065dc53387d3d3cf
Peng <pengh@pengh-wm2.janelia.priv>
2010-06-06 18:25:56 -0400

    add some simple definition of the dialog

---
+++ Source/V3DInterface/v3d_simple_parameter_diag.h
@@ -0,0 +1,70 @@
+#ifndef __V3D_SIMPLE_PARAMETER_DIALOG_H__
+#define __V3D_SIMPLE_PARAMETER_DIALOG_H__
+
+#include <QtGui>
+
+struct _simpleQtInputLine
+{
+	QString labelText;
+	void * qtObj;
+	_simpleQtInputLine() {labelText=""; qtObj=0;}
+	~_simpleQtInputLine() {if (qtObj) {delete qtObj; qtObj=0;} }
+};
+
+class V3DSimpleParameterDialog: QDialog
+{
+public:
+    enum RowInputMode {
+        TextInput,
+        IntInput,
+        DoubleInput,
+		BoolInput,
+		ComboInput
+    };
+	
+	bool addInputRow(QString & labelText, RowInputMode m, QVariant & v)
+	{
+		_simpleQtInputLine s;
+		switch (m)
+		{
+			case TextInput:
+				s.labelText = labelText;
+				s.qtObj = (void *) new Q
+				break;
+				
+			case IntInput:
+				break;
+
+			case DoubleInput:
+				break;
+				
+			case BoolInput:
+				s.labelText = labelText;
+				s.qtObj = (void *) new QCheckBox(labelText, this);
+				((QCheckBox *)s.qtObj)->setCheckState( (v.toBool() ) ? Qt::Checked : Qt::Unchecked() );
+				break;
+				
+			case ComboInput:
+				s.labelText = labelText;
+				s.qtObj = (void *) new QComboBox(labelText, this);
+				((QCheckBox *)s.qtObj)->setCheckState( (v.toBool() ) ? Qt::Checked : Qt::Unchecked() );
+				break;
+				
+			default:
+				return false;
+				break;
+		}
+		return false;
+	}
+	
+	QVariant getValue(QString & variableName)
+	{
+	}
+
+protected:
+	QList <_simpleQtInputLine> entryList;
+	QMap <QString, _simpleQtInputLine> map;
+};
+
+#endif
+
