629104614a9c332f72a16b198b165f409e6530dd
Luis Ibanez <luis.ibanez@kitware.com>
2010-07-29 18:24:13 -0400

    Converted to modern Plugin hierarchy and qt dialog.
    This filter is now only computing the watershed and
    relabeling the output image to an image of pixel type
    unsigned short int.

--- Source/Watershed/ITKWatershed.cxx
+++ Source/Watershed/ITKWatershed.cxx
@@ -1,7 +1,3 @@
-/* ITKWatershed.cxx
- * 2010-06-03: create this program by Yang Yu
- */
-
 #include <QtGui>
 
 #include <math.h>
@@ -11,287 +7,143 @@
 #include "V3DITKFilterSingleImage.h"
 
 // ITK Header Files
-#include "itkImage.h"
-
-#include "itkImportImageFilter.h"
-#include "itkCastImageFilter.h"
-#include "itkRelabelComponentImageFilter.h"
-
-#include "itkGradientMagnitudeRecursiveGaussianImageFilter.h"
 #include "itkWatershedImageFilter.h"
+#include "itkRelabelComponentImageFilter.h"
+#include "itkImageFileWriter.h"
+
 
 // Q_EXPORT_PLUGIN2 ( PluginName, ClassName )
 // The value of PluginName should correspond to the TARGET specified in the
 // plugin's project file.
-Q_EXPORT_PLUGIN2(ITKWatershed, ITKWatershedPlugin)
+Q_EXPORT_PLUGIN2(Watershed, WatershedPlugin)
 
-//plugin funcs
-const QString title = "ITK Watershed";
-QStringList ITKWatershedPlugin::menulist() const
+
+QStringList WatershedPlugin::menulist() const
 {
-	return QStringList() << QObject::tr("ITK Watershed")
-						 << QObject::tr("about this plugin");
+    return QStringList() << QObject::tr("ITK Watershed")
+            << QObject::tr("about this plugin");
 }
 
-QStringList ITKWatershedPlugin::funclist() const
+QStringList WatershedPlugin::funclist() const
 {
     return QStringList();
 }
 
-void ITKWatershedPlugin::dofunc(const QString & func_name,
-											const V3DPluginArgList & input, V3DPluginArgList & output, QWidget * parent)
-{
-	// empty by now
-}
 
-
-template <typename TInputPixelType, typename TOutputPixelType>
-class ITKWatershedSpecializaed // : public V3DITKFilterSingleImage< TInputPixelType, TOutputPixelType >
+template <typename TPixelType>
+class PluginSpecialized : public V3DITKFilterSingleImage< TPixelType, unsigned short int >
 {
+  typedef V3DITKFilterSingleImage< TPixelType, unsigned short int >   Superclass;
+  typedef typename Superclass::Input3DImageType                       ImageType;
+
+  typedef itk::Image< unsigned short int, 3 > OutputImageType;
+
+  typedef itk::WatershedImageFilter< ImageType > WatershedFilterType;
+  typedef typename WatershedFilterType::OutputImageType  LabeledImageType;
+  typedef itk::RelabelComponentImageFilter< LabeledImageType, OutputImageType > RelabelComponentImageFilterType;
+
 public:
-	//typedef V3DITKFilterSingleImage< TInputPixelType, TOutputPixelType >    Superclass;
-	
-	//ITKWatershedSpecializaed( V3DPluginCallback * callback ): Superclass(callback) {}
-	virtual ~ITKWatershedSpecializaed() {};
-	
-	//
-	void Execute(V3DPluginCallback &callback, QWidget *parent)
-	{
-		v3dhandle curwin = callback.currentImageWindow();
-		if (!curwin)
-		{
-			v3d_msg(QObject::tr("You don't have any image open in the main window."));
-			return;
-		}
-		
-		Image4DSimple *p4DImage = callback.getImage(curwin);
-		if (! p4DImage)
-		{
-			v3d_msg(QObject::tr("The input image is null."));
-			return;
-		}	
-			
-		V3D_GlobalSetting globalSetting = callback.getGlobalSetting();
-		
-		
-		//init
-		typedef TInputPixelType  PixelType;
-		
-		PixelType * data1d = reinterpret_cast< PixelType * >( p4DImage->getRawData() );
-		unsigned long int numberOfPixels = p4DImage->getTotalBytes();
-		
-		long pagesz = p4DImage->getTotalUnitNumberPerChannel();
-		
-		long nx = p4DImage->getXDim();
-		long ny = p4DImage->getYDim();
-		long nz = p4DImage->getZDim();
-		long nc = p4DImage->getCDim();  // Number of channels
-		
-		int channelToFilter = globalSetting.iChannel_for_plugin;
-		
-		if( channelToFilter >= nc )
-		{
-			v3d_msg(QObject::tr("You are selecting a channel that doesn't exist in this image."));
-			return;
-		}
-		
-		long offsets=0; 
-		if(channelToFilter>=0) offsets = channelToFilter*pagesz; 
-		
-		const unsigned int Dimension = 3; // \par
-		
-		typedef itk::Image< TInputPixelType, Dimension > InputImageType;
-		typedef itk::Image< TOutputPixelType, Dimension > OutputImageType;
-		typedef itk::ImportImageFilter< TInputPixelType, Dimension > ImportFilterType;
-		
-		typename ImportFilterType::Pointer importFilter = ImportFilterType::New();
-		
-		typename ImportFilterType::SizeType size;
-		size[0] = nx;
-		size[1] = ny;
-		size[2] = nz;
-		
-		typename ImportFilterType::IndexType start;
-		start.Fill( 0 );
-		
-		typename ImportFilterType::RegionType region;
-		region.SetIndex( start );
-		region.SetSize(  size  );
-		
-		importFilter->SetRegion( region );
-		
-		region.SetSize( size );
-		
-		typename InputImageType::PointType origin;
-		origin.Fill( 0.0 );
-		
-		importFilter->SetOrigin( origin );
-		
-		typename ImportFilterType::SpacingType spacing;
-		spacing.Fill( 1.0 );
-		
-		importFilter->SetSpacing( spacing );
-		
-		const bool importImageFilterWillOwnTheBuffer = false;
-		
-		//Declaration of fileters
-		typedef   itk::GradientMagnitudeRecursiveGaussianImageFilter< InputImageType, OutputImageType > GradientMagnitudeFilterType;
-		typename GradientMagnitudeFilterType::Pointer gradienMagnitudeFilter = GradientMagnitudeFilterType::New();
-		
-		typedef  itk::WatershedImageFilter< OutputImageType > WatershedFilterType;
-		typename WatershedFilterType::Pointer watershedFilter = WatershedFilterType::New();
-		
-		typedef typename WatershedFilterType::OutputImageType  LabeledImageType;
-		
-		typedef itk::RelabelComponentImageFilter< LabeledImageType, OutputImageType > RelabelComponentImageFilterType;
-		typename RelabelComponentImageFilterType::Pointer relabelComponent = RelabelComponentImageFilterType::New();
-		
-		//set \pars		
-		
-		//consider multiple channels
-		if(channelToFilter==-1)
-		{
-			TOutputPixelType *output1d;
-			try
-			{
-				output1d = new TOutputPixelType [numberOfPixels];
-			}
-			catch(...)
-			{
-				std::cerr << "Error memroy allocating." << std::endl;
-				return;
-			}
-			
-			const bool filterWillDeleteTheInputBuffer = false;
-			
-			for(long ch=0; ch<nc; ch++)
-			{
-				offsets = ch*pagesz;
-				
-				TOutputPixelType *p = output1d+offsets;
-				
-				importFilter->SetImportPointer( data1d+offsets, pagesz, importImageFilterWillOwnTheBuffer );
-				
-				// watershed
-				gradienMagnitudeFilter->SetInput( importFilter->GetOutput() );
-				
-				watershedFilter->SetInput( gradienMagnitudeFilter->GetOutput() );
-				relabelComponent->SetInput( watershedFilter->GetOutput() );
-				
-				gradienMagnitudeFilter->SetSigma( 1.0 );
-				
-				watershedFilter->SetThreshold( 100.0 );
-				watershedFilter->SetLevel( 127.5 );
-				
-				//watershedSegmentation
-				relabelComponent->GetOutput()->GetPixelContainer()->SetImportPointer( p, pagesz, filterWillDeleteTheInputBuffer);
-				
-				try
-				{
-					relabelComponent->Update();
-				}
-				catch( itk::ExceptionObject & excp)
-				{
-					std::cerr << "Error run this filter." << std::endl;
-					std::cerr << excp << std::endl;
-					return;
-				}
-				
-			}
-			
-			setPluginOutputAndDisplayUsingGlobalSetting(output1d, nx, ny, nz, nc, callback);
-		}
-		else if(channelToFilter<nc)
-		{
-			importFilter->SetImportPointer( data1d+offsets, pagesz, importImageFilterWillOwnTheBuffer );
-			
-			// watershed
-			gradienMagnitudeFilter->SetInput( importFilter->GetOutput() );
-			
-			watershedFilter->SetInput( gradienMagnitudeFilter->GetOutput() );
-			relabelComponent->SetInput( watershedFilter->GetOutput() );
-			
-			gradienMagnitudeFilter->SetSigma( 1.0 );
-			
-			watershedFilter->SetThreshold( 0.0 ); // (0.0, 1.0)
-			watershedFilter->SetLevel( 0.05 ); // (0.0, 1.0)
-			
-			//watershedSegmentation
-			try
-			{
-				relabelComponent->Update();
-			}
-			catch( itk::ExceptionObject & excp)
-			{
-				std::cerr << "Error run this filter." << std::endl;
-				std::cerr << excp << std::endl;
-				return;
-			}
-			
-			// output
-			typename OutputImageType::PixelContainer * container;
-			
-			container =relabelComponent->GetOutput()->GetPixelContainer();
-			container->SetContainerManageMemory( false );
-			
-			typedef TOutputPixelType OutputPixelType;
-			OutputPixelType * output1d = container->GetImportPointer();
-			
-			setPluginOutputAndDisplayUsingGlobalSetting(output1d, nx, ny, nz, 1, callback);
-			
-		}
-		
-	} //Execute	
-		
-	
+
+  PluginSpecialized( V3DPluginCallback * callback ): Superclass(callback)
+    {
+    this->m_Filter = WatershedFilterType::New();
+    this->m_RelabelFilter = RelabelComponentImageFilterType::New();
+    this->m_RelabelFilter->SetInput( this->m_Filter->GetOutput() );
+    }
+
+  virtual ~PluginSpecialized() {};
+
+
+  void Execute(const QString &menu_name, QWidget *parent)
+    {
+    V3DITKGenericDialog dialog("Watershed");
+
+    dialog.AddDialogElement("Threshold",0.01, 0.0, 1.0);
+    dialog.AddDialogElement("WaterLevel",0.2, 0.0, 1.0);
+
+    if( dialog.exec() == QDialog::Accepted )
+      {
+      this->m_Filter->SetThreshold( dialog.GetValue("Threshold") );
+      this->m_Filter->SetLevel( dialog.GetValue("WaterLevel") );
+
+      this->Compute();
+      }
+    }
+
+  virtual void ComputeOneRegion()
+    {
+
+    this->m_Filter->SetInput( this->GetInput3DImage() );
+
+    this->m_RelabelFilter->Update();
+
+    {
+    typedef itk::ImageFileWriter< LabeledImageType > WriterType;
+    typename WriterType::Pointer writer = WriterType::New();
+    writer->SetFileName("WatershedOutput.mhd");
+    writer->SetInput( this->m_Filter->GetOutput() );
+    writer->Update();
+    }
+
+    {
+    typedef itk::ImageFileWriter< OutputImageType > WriterType;
+    typename WriterType::Pointer writer = WriterType::New();
+    writer->SetFileName("WatershedOutputRelabeled.mhd");
+    writer->SetInput( this->m_RelabelFilter->GetOutput() );
+    writer->Update();
+    }
+
+
+    this->SetOutputImage( this->m_RelabelFilter->GetOutput() );
+    }
+
+
+private:
+
+    typename WatershedFilterType::Pointer   m_Filter;
+    typename RelabelComponentImageFilterType::Pointer   m_RelabelFilter;
+
 };
 
-#define EXECUTE( v3d_pixel_type, input_pixel_type, output_pixel_type ) \
-	case v3d_pixel_type: \
-	{ \
-		ITKWatershedSpecializaed< input_pixel_type, output_pixel_type > runner; \
-		runner.Execute( callback, parent ); \
-		break; \
-	} 
 
-#define EXECUTE_ALL_PIXEL_TYPES \
-	ImagePixelType pixelType = p4DImage->getDatatype(); \
-	switch( pixelType )  \
-	{  \
-		EXECUTE( V3D_UINT8, unsigned char, float );  \
-		EXECUTE( V3D_UINT16, unsigned short int, float );  \
-		EXECUTE( V3D_FLOAT32, float, float );  \
-		case V3D_UNKNOWN:  \
-		{  \
-		}  \
-	}  
-
-
-void ITKWatershedPlugin::domenu(const QString & menu_name, V3DPluginCallback & callback, QWidget * parent)
-{
-    if (menu_name == QObject::tr("ITK Watershed"))
-    {
-    	v3dhandle curwin = callback.currentImageWindow();
-		if (!curwin)
-		{
-			v3d_msg(QObject::tr("You don't have any image open in the main window."));
-			return;
-		}
-		
-		Image4DSimple *p4DImage = callback.getImage(curwin);
-		if (! p4DImage)
-		{
-			v3d_msg(QObject::tr("The input image is null."));
-			return;
-		}	
-		
-		EXECUTE_ALL_PIXEL_TYPES;
+#define EXECUTE_PLUGIN_FOR_ONE_IMAGE_TYPE( v3d_pixel_type, c_pixel_type ) \
+  case v3d_pixel_type: \
+    { \
+    PluginSpecialized< c_pixel_type > runner( &callback ); \
+    runner.Execute( menu_name, parent ); \
+    break; \
     }
-	else if (menu_name == QObject::tr("about this plugin"))
-	{
-		QMessageBox::information(parent, "Version info", "ITK Watershed 1.0 (2010-June-04): this plugin is developed by Yang Yu.");
-	}
+
+
+void WatershedPlugin::dofunc(const QString & func_name,
+    const V3DPluginArgList & input, V3DPluginArgList & output, QWidget * parent)
+{
+  // empty by now
 }
 
 
+void WatershedPlugin::domenu(const QString & menu_name, V3DPluginCallback & callback, QWidget * parent)
+{
+  if (menu_name == QObject::tr("about this plugin"))
+    {
+    QMessageBox::information(parent, "Version info", "ITK Watershed 1.0 (2010-Jun-21): this plugin is developed by Luis Ibanez.");
+    return;
+    }
+
+  v3dhandle curwin = callback.currentImageWindow();
+  if (!curwin)
+    {
+    v3d_msg(tr("You don't have any image open in the main window."));
+    return;
+    }
+
+  Image4DSimple *p4DImage = callback.getImage(curwin);
+  if (! p4DImage)
+    {
+    v3d_msg(tr("The input image is null."));
+    return;
+    }
+
+  EXECUTE_PLUGIN_FOR_ALL_PIXEL_TYPES;
+}
 
--- Source/Watershed/ITKWatershed.h
+++ Source/Watershed/ITKWatershed.h
@@ -1,12 +1,6 @@
-/* ITKWatershed.h
- * 2010-06-03: create this program by Yang Yu
- */
+#ifndef __Watershed_H__
+#define __Watershed_H__
 
-#ifndef __ITKTHRESHOLDSEGMENTATION_H__
-#define __ITKTHRESHOLDSEGMENTATION_H__
-
-//   WATERSHED SEGMENTATION.
-//
 
 #include <QtGui>
 #include <stdio.h>
@@ -15,21 +9,21 @@
 #include <math.h>
 #include <v3d_interface.h>
 
-class ITKWatershedPlugin : public QObject, public V3DPluginInterface
+class WatershedPlugin : public QObject, public V3DPluginInterface
 {
     Q_OBJECT
-    Q_INTERFACES(V3DPluginInterface);
-	
-public:
-	ITKWatershedPlugin() {}
-	QStringList menulist() const;
-	QStringList funclist() const;
-	void domenu(const QString &menu_name, V3DPluginCallback &callback, QWidget *parent);
-	
-	virtual void dofunc(const QString & func_name,
-						const V3DPluginArgList & input, V3DPluginArgList & output, QWidget * parent);
-	
-};
+    Q_INTERFACES(V3DPluginInterface)
 
+public:
+	WatershedPlugin() {}
+  QStringList menulist() const;
+	QStringList funclist() const;
+
+	void domenu(const QString & menu_name, V3DPluginCallback & callback, QWidget * parent);
+
+	virtual void dofunc(const QString & func_name,
+			const V3DPluginArgList & input, V3DPluginArgList & output, QWidget * parent);
+
+};
 
 #endif
