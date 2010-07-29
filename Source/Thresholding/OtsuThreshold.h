5519693f721d400309bad1d10acc16efc0047439
Luis Ibanez <luis.ibanez@kitware.com>
2010-07-29 01:56:40 -0400

    Convert it to modern plugin structure

--- Source/Thresholding/OtsuThreshold.h
+++ Source/Thresholding/OtsuThreshold.h
@@ -1,9 +1,6 @@
-/* OtsuThreshold.h
- * 2010-06-02: create this program by Lei Qu
- */
+#ifndef __OtsuThreshold_H__
+#define __OtsuThreshold_H__
 
-#ifndef __OTSUTHRESHOLD_H__
-#define __OTSUTHRESHOLD_H__
 
 #include <QtGui>
 #include <stdio.h>
@@ -12,58 +9,21 @@
 #include <math.h>
 #include <v3d_interface.h>
 
-class ITKOtsuThresholdPlugin: public QObject, public V3DPluginInterface
+class OtsuThresholdPlugin : public QObject, public V3DPluginInterface
 {
-	Q_OBJECT
-	Q_INTERFACES(V3DPluginInterface)
+    Q_OBJECT
+    Q_INTERFACES(V3DPluginInterface)
 
 public:
-	QStringList menulist() const;
-	void domenu(const QString &menu_name, V3DPluginCallback &callback, QWidget *parent);
+	OtsuThresholdPlugin() {}
+  QStringList menulist() const;
+	QStringList funclist() const;
 
-	QStringList funclist() const {return QStringList();}
-	void dofunc(const QString &func_name, const V3DPluginArgList &input, V3DPluginArgList &output, QWidget *parent) {}
+	void domenu(const QString & menu_name, V3DPluginCallback & callback, QWidget * parent);
 
-};
+	virtual void dofunc(const QString & func_name,
+			const V3DPluginArgList & input, V3DPluginArgList & output, QWidget * parent);
 
-class ITKOtsuThresholdDialog: public QDialog
-{
-Q_OBJECT
-
-public:
-	ITKOtsuThresholdDialog(Image4DSimple *p4DImage, QWidget *parent)
-	{
-		if (!p4DImage)
-			return;
-
-		printf("Passing data to data1d\n");
-
-		ok = new QPushButton("OK");
-		cancel = new QPushButton("Cancel");
-
-		gridLayout = new QGridLayout();
-
-		gridLayout->addWidget(cancel, 0, 0);
-		gridLayout->addWidget(ok, 0, 1);
-		setLayout( gridLayout);
-		setWindowTitle(QString("Otsu Threshold"));
-
-		connect(ok, SIGNAL(clicked()), this, SLOT(accept()));
-		connect(cancel, SIGNAL(clicked()), this, SLOT(reject()));
-	}
-
-	~ITKOtsuThresholdDialog()
-	{
-	}
-
-public slots:
-
-public:
-	QGridLayout *gridLayout;
-
-	QPushButton* ok;
-	QPushButton* cancel;
 };
 
 #endif
-
