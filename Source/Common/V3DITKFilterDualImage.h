55a2e70f3be6230945d7b22431595f46b43df098
Luis Ibanez <luis.ibanez@kitware.com>
2010-07-30 22:17:23 -0400

    Added Factrization with V3DITKFilterBaseImage
    The common features of the classes V3DITKFilterSingleImage and V3DITKFilterDualImage
    were factorized into V3DITKFilterBaseImage.

--- Source/Common/V3DITKFilterDualImage.h
+++ Source/Common/V3DITKFilterDualImage.h
@@ -1,58 +1,29 @@
 #ifndef __V3DITKFilterDualImage_H__
 #define __V3DITKFilterDualImage_H__
 
-#include <v3d_interface.h>
-#include "V3DITKGenericDialog.h"
-#include "itkImage.h"
-#include "itkImportImageFilter.h"
-
-
-#define EXECUTE_PLUGIN_FOR_ALL_PIXEL_TYPES \
-    ImagePixelType pixelType = p4DImage->getDatatype(); \
-    switch( pixelType )  \
-      {  \
-      EXECUTE_PLUGIN_FOR_ONE_IMAGE_TYPE( V3D_UINT8, unsigned char );  \
-      EXECUTE_PLUGIN_FOR_ONE_IMAGE_TYPE( V3D_UINT16, unsigned short int );  \
-      EXECUTE_PLUGIN_FOR_ONE_IMAGE_TYPE( V3D_FLOAT32, float );  \
-      case V3D_UNKNOWN:  \
-        {  \
-        }  \
-      }
-
+#include "V3DITKFilterBaseImage.h"
 
 template <typename TInputPixelType, typename TOutputPixelType>
-class V3DITKFilterDualImage
+class V3DITKFilterDualImage : public V3DITKFilterBaseImage < TInputPixelType, TOutputPixelType >
 {
 public:
 
-  typedef TInputPixelType                   InputPixelType;
-  typedef itk::Image< InputPixelType, 2 >   Input2DImageType;
-  typedef itk::Image< InputPixelType, 3 >   Input3DImageType;
+  typedef V3DITKFilterBaseImage< TInputPixelType, TOutputPixelType >  Superclass;
 
-  typedef TOutputPixelType                  OutputPixelType;
-  typedef itk::Image< OutputPixelType, 2 >  Output2DImageType;
-  typedef itk::Image< OutputPixelType, 3 >  Output3DImageType;
+  typedef typename Superclass::Input2DImageType   Input2DImageType;
+  typedef typename Superclass::Input3DImageType   Input3DImageType;
 
-public:
+  typedef typename Superclass::Output2DImageType   Output2DImageType;
+  typedef typename Superclass::Output3DImageType   Output3DImageType;
 
   V3DITKFilterDualImage( V3DPluginCallback * callback );
   virtual ~V3DITKFilterDualImage();
 
-  void Execute(const QString &menu_name, QWidget *parent);
-
 protected:
 
+  virtual void TransferInputImages( V3DPluginCallback * callback );
+
   virtual void Compute();
-  virtual void Initialize();
-
-  virtual void TransferInputImages( V3DPluginCallback & callback );
-
-  // FIXME: Should the TransferInput() method be removed ?
-  virtual void TransferInput( const V3D_Image3DBasic & inputImage,
-    V3DLONG x1, V3DLONG x2, V3DLONG y1, V3DLONG y2, V3DLONG z1, V3DLONG z2 );
-
-  virtual void ComputeOneRegion() = 0;  //this needs to be implemented for new plugin code
-  virtual void TransferOutput( V3D_Image3DBasic & outputImage ) const;
 
   const Input2DImageType * GetInput2DImage1() const;
   const Input2DImageType * GetInput2DImage2() const;
@@ -60,36 +31,16 @@ protected:
   const Input3DImageType * GetInput3DImage1() const;
   const Input3DImageType * GetInput3DImage2() const;
 
-  void SetOutputImage( Output2DImageType * image );
-  void SetOutputImage( Output3DImageType * image );
-
-  bool ShouldGenerateNewWindow() const;
-
 private:
 
-  typedef itk::ImportImageFilter< InputPixelType, 2 > Import2DFilterType;
-  typedef itk::ImportImageFilter< InputPixelType, 3 > Import3DFilterType;
+  typedef typename Superclass::Import2DFilterType  Import2DFilterType;
+  typedef typename Superclass::Import3DFilterType  Import3DFilterType;
 
   typename Import2DFilterType::Pointer      m_Impor2DFilter1;
   typename Import2DFilterType::Pointer      m_Impor2DFilter2;
 
   typename Import3DFilterType::Pointer      m_Impor3DFilter1;
   typename Import3DFilterType::Pointer      m_Impor3DFilter2;
-
-  V3DPluginCallback  *                      m_V3DPluginCallback;
-
-  v3dhandle                                 m_CurrentWindow;
-
-  V3D_GlobalSetting                         m_GlobalSetting;
-
-  Image4DSimple *                           m_4DImage;
-
-  InputPixelType *                          m_Data1D;
-
-  V3DLONG                                   m_NumberOfPixelsAlongX;
-  V3DLONG                                   m_NumberOfPixelsAlongY;
-  V3DLONG                                   m_NumberOfPixelsAlongZ;
-  V3DLONG                                   m_NumberOfChannels;
 
   typename Output2DImageType::Pointer       m_Output2DImage;
   typename Output3DImageType::Pointer       m_Output3DImage;
