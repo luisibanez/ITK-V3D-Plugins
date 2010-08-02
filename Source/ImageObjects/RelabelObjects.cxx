ea53adb613784bed16787eeb7065c0e6d1d8a8f2
Luis Ibanez <luis.ibanez@kitware.com>
2010-08-02 16:34:18 -0400

    Fixing Relabel and RelabelObject plugin.
    The Relabel plugin was renamed as RelabelObject, but was not removed
    from the repository. Now, we take it out. The RelabelObjects plugin
    is now using the progress reporting mechanism and the Qt dialog for
    requesting numeric parameters, in particular the minimum object size
    that should be considered when relabeling the connected components.

--- Source/ImageObjects/RelabelObjects.cxx
+++ Source/ImageObjects/RelabelObjects.cxx
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <stdlib.h>
 
-#include "Relabel.h"
+#include "RelabelObjects.h"
 #include "V3DITKFilterSingleImage.h"
 
 // ITK Header Files
@@ -19,7 +19,7 @@ Q_EXPORT_PLUGIN2(RelabelObjects, RelabelObjectsPlugin)
 QStringList RelabelObjectsPlugin::menulist() const
 {
     return QStringList() << QObject::tr("Relabel Indexed Image Objects")
-	<< QObject::tr("about this plugin");
+  << QObject::tr("about this plugin");
 }
 
 QStringList RelabelObjectsPlugin::funclist() const
@@ -29,101 +29,101 @@ QStringList RelabelObjectsPlugin::funclist() const
 
 
 template <typename TInPixelType, typename TOutputPixelType>
-class MySpecialized : public V3DITKFilterSingleImage< TInPixelType, TOutputPixelType >
+class PluginSpecialized : public V3DITKFilterSingleImage< TInPixelType, TOutputPixelType >
 {
-	typedef V3DITKFilterSingleImage< TInPixelType, TOutputPixelType >   Superclass;
-	typedef typename Superclass::Input3DImageType               ImageType;
-	typedef typename Superclass::Output3DImageType              OutputImageType;
-	
-	typedef itk::RelabelComponentImageFilter< ImageType, OutputImageType > FilterType;
-	
+  typedef V3DITKFilterSingleImage< TInPixelType, TOutputPixelType >   Superclass;
+  typedef typename Superclass::Input3DImageType               ImageType;
+  typedef typename Superclass::Output3DImageType              OutputImageType;
+
+  typedef itk::RelabelComponentImageFilter< ImageType, OutputImageType > FilterType;
+
 public:
-	
-	MySpecialized( V3DPluginCallback * callback ): Superclass(callback)
+
+  PluginSpecialized( V3DPluginCallback * callback ): Superclass(callback)
     {
-		this->m_Filter = FilterType::New();
+    this->m_Filter = FilterType::New();
+    this->RegisterInternalFilter( this->m_Filter, 1.0 );
     }
-	
-	virtual ~MySpecialized() {};
-	
-	
-	void Execute(const QString &menu_name, QWidget *parent)
+
+  virtual ~PluginSpecialized() {};
+
+
+  void Execute(const QString &menu_name, QWidget *parent)
     {
-		this->Compute(); 
+    V3DITKGenericDialog dialog("Connected Components");
+
+    dialog.AddDialogElement("MinimumObjectSize",27.0, 1.0, 60000.0); // This should be an integer
+
+    if( dialog.exec() == QDialog::Accepted )
+      {
+      this->m_Filter->SetMinimumObjectSize( dialog.GetValue("MinimumObjectSize") ); // This should be an integer
+
+      this->Compute();
+      }
+
     }
-	
-	virtual void ComputeOneRegion()
+
+  virtual void ComputeOneRegion()
     {
-		
-		this->m_Filter->SetInput( this->GetInput3DImage() );
-		
-		if( !this->ShouldGenerateNewWindow() )
-		{
-			this->m_Filter->InPlaceOn();
-		}
-		
-		this->m_Filter->Update();
-		
-		this->SetOutputImage( this->m_Filter->GetOutput() );
+
+    this->m_Filter->SetInput( this->GetInput3DImage() );
+
+    if( !this->ShouldGenerateNewWindow() )
+    {
+      this->m_Filter->InPlaceOn();
     }
-	
-	virtual void SetupParameters()
-  	{
-		//
-		// These values should actually be provided by the Qt Dialog...
-		//
-		
-		// just search the respective .h file for the itkSetMacro for parameters
-		
-		this->m_Filter->SetMinimumObjectSize( 27 );  //the least 26 voxels
-		this->m_Filter->SetNumberOfObjectsToPrint( 100 ); //only print 100 objects
-	}
-	
+
+    this->m_Filter->Update();
+
+    this->SetOutputImage( this->m_Filter->GetOutput() );
+    }
+
+
 private:
-	
+
     typename FilterType::Pointer   m_Filter;
-	
+
 };
 
 
 #define EXECUTE_PLUGIN_FOR_ONE_IMAGE_TYPE( v3d_pixel_type, c_pixel_type ) \
-case v3d_pixel_type: \
-{ \
-MySpecialized< c_pixel_type, c_pixel_type > runner( &callback ); \
-runner.Execute( menu_name, parent ); \
-break; \
-} 
+  case v3d_pixel_type: \
+    { \
+    PluginSpecialized< c_pixel_type, c_pixel_type > runner( &callback ); \
+    runner.Execute( menu_name, parent ); \
+    break; \
+    }
 
 
 void RelabelObjectsPlugin::dofunc(const QString & func_name,
-									 const V3DPluginArgList & input, V3DPluginArgList & output, QWidget * parent)
+    const V3DPluginArgList & input, V3DPluginArgList & output, QWidget * parent)
 {
-	// empty by now
+  // empty by now
 }
 
 
 void RelabelObjectsPlugin::domenu(const QString & menu_name, V3DPluginCallback & callback, QWidget * parent)
 {
-	if (menu_name == QObject::tr("about this plugin"))
+  if (menu_name == QObject::tr("about this plugin"))
     {
-		QMessageBox::information(parent, "Version info", "ITK Relabel Image Objects (Connnected Components) 1.0 (2010-June-4): this plugin is developed by Hanchuan Peng.");
-		return;
+    QMessageBox::information(parent, "Version info", "ITK Relabel Image Objects (Connnected Components) 1.0 (2010-June-4): this plugin is developed by Hanchuan Peng.");
+    return;
     }
-	
-	v3dhandle curwin = callback.currentImageWindow();
-	if (!curwin)
+
+  v3dhandle curwin = callback.currentImageWindow();
+  if (!curwin)
     {
-		v3d_msg(tr("You don't have any image open in the main window."));
-		return;
+    v3d_msg(tr("You don't have any image open in the main window."));
+    return;
     }
-	
-	Image4DSimple *p4DImage = callback.getImage(curwin);
-	if (! p4DImage)
+
+  Image4DSimple *p4DImage = callback.getImage(curwin);
+  if (! p4DImage)
     {
-		v3d_msg(tr("The input image is null."));
-		return;
+    v3d_msg(tr("The input image is null."));
+    return;
     }
-	
-	EXECUTE_PLUGIN_FOR_ALL_PIXEL_TYPES; 
+
+  EXECUTE_PLUGIN_FOR_ALL_PIXEL_TYPES;
 }
 
--- Source/ImageObjects/RelabelObjects.h
+++ Source/ImageObjects/RelabelObjects.h
@@ -1,29 +1,13 @@
 #ifndef __RelabelObjects_H__
 #define __RelabelObjects_H__
 
-
-#include <QtGui>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <v3d_interface.h>
+#include "V3DITKPluginDefaultHeader.h"
 
 class RelabelObjectsPlugin : public QObject, public V3DPluginInterface
 {
-    Q_OBJECT
-    Q_INTERFACES(V3DPluginInterface)
-
-public:
-	RelabelObjectsPlugin() {}
-    QStringList menulist() const;
-	QStringList funclist() const;
-
-	void domenu(const QString & menu_name, V3DPluginCallback & callback, QWidget * parent);
-
-	virtual void dofunc(const QString & func_name,
-			const V3DPluginArgList & input, V3DPluginArgList & output, QWidget * parent);
-
+  Q_OBJECT
+  Q_INTERFACES(V3DPluginInterface)
+  V3DITKPLUGIN_DEFAULT_CLASS_DECLARATION_BODY(RelabelObjects);
 };
 
 #endif
