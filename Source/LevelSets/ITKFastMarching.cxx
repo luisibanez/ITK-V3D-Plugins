8bc8046726da7dc2a44497dae627b4a65dca18ec
Luis Ibanez <luis.ibanez@kitware.com>
2010-08-03 02:08:52 -0400

    Clonned the FastMarching filter to a simple filter.
    A variation of the FastMarching plugin is introduced here in order
    to facilitate debugging.

--- Source/LevelSets/ITKFastMarching.cxx
+++ Source/LevelSets/ITKFastMarching.cxx
@@ -1,431 +1,166 @@
-/* ITKFastMarching.cxx
- * 2010-06-02: create this program by Yang Yu
- */
-
 #include <QtGui>
 
 #include <math.h>
 #include <stdlib.h>
 
 #include "ITKFastMarching.h"
+#include "V3DITKFilterSingleImage.h"
 
 // ITK Header Files
-#include "itkImage.h"
 #include "itkFastMarchingImageFilter.h"
-#include "itkImportImageFilter.h"
-#include "itkCastImageFilter.h"
 
-#include "itkCurvatureAnisotropicDiffusionImageFilter.h"
-//#include "itkGradientAnisotropicDiffusionImageFilter.h"
-#include "itkGradientMagnitudeRecursiveGaussianImageFilter.h"
-#include "itkSigmoidImageFilter.h"
-#include "itkBinaryThresholdImageFilter.h"
-#include "itkRescaleIntensityImageFilter.h"
 
 // Q_EXPORT_PLUGIN2 ( PluginName, ClassName )
 // The value of PluginName should correspond to the TARGET specified in the
 // plugin's project file.
-Q_EXPORT_PLUGIN2(ITKFastMarching, ITKFastMarchingPlugin)
+Q_EXPORT_PLUGIN2(FastMarching, FastMarchingPlugin)
 
-void itkFastMarchingPlugin(V3DPluginCallback &callback, QWidget *parent);
 
-//plugin funcs
-const QString title = "ITK FastMarching";
-QStringList ITKFastMarchingPlugin::menulist() const
+QStringList FastMarchingPlugin::menulist() const
 {
-	return QStringList() << QObject::tr("ITK FastMarching")
-	<< QObject::tr("about this plugin");
+    return QStringList() << QObject::tr("ITK FastMarching")
+            << QObject::tr("about this plugin");
 }
 
-void ITKFastMarchingPlugin::domenu(const QString &menu_name, V3DPluginCallback &callback, QWidget *parent)
+QStringList FastMarchingPlugin::funclist() const
 {
-    if (menu_name == QObject::tr("ITK FastMarching"))
-    {
-    	itkFastMarchingPlugin(callback, parent);
-    }
-	else if (menu_name == QObject::tr("about this plugin"))
-	{
-		QMessageBox::information(parent, "Version info", "ITK Fast Marching 1.0 (2010-June-02): this plugin is developed by Yang Yu.");
-	}
+    return QStringList();
 }
 
 
-template <typename TInputPixelType, typename TOutputPixelType>
-class ITKFastMarchingSpecializaed
+template <typename TPixelType>
+class PluginSpecialized : public V3DITKFilterSingleImage< TPixelType, TPixelType >
 {
+  typedef V3DITKFilterSingleImage< TPixelType, TPixelType >   Superclass;
+  typedef typename Superclass::Input3DImageType               ImageType;
+
+  typedef itk::FastMarchingImageFilter< ImageType, ImageType > FilterType;
+
 public:
-	void Execute(V3DPluginCallback &callback, QWidget *parent)
-	{
-		//
-		ITKFastMarchingDialog d(callback, parent);
-		
-		//
-		if (d.exec()!=QDialog::Accepted)
-		{
-			return;
-		}
-		else
-		{
-			//passing \pars
-			d.update();
-			
-			int i1 = d.i1;
-			
-			//ori image
-			v3dhandleList win_list = callback.getImageWindowList();
-			
-			V3D_GlobalSetting globalSetting = callback.getGlobalSetting();
-			Image4DSimple *p4DImage = callback.getImage(win_list[i1]);
-			
-			//init
-			typedef TInputPixelType  PixelType;
-			
-			PixelType * data1d = reinterpret_cast< PixelType * >( p4DImage->getRawData() );
-			unsigned long int numberOfPixels = p4DImage->getTotalBytes();
-			
-			long pagesz = p4DImage->getTotalUnitNumberPerChannel();
-			
-			long nx = p4DImage->getXDim();
-			long ny = p4DImage->getYDim();
-			long nz = p4DImage->getZDim();
-			long nc = p4DImage->getCDim();  // Number of channels
-			
-			int channelToFilter = globalSetting.iChannel_for_plugin;
-			
-			if( channelToFilter >= nc )
-			{
-				v3d_msg(QObject::tr("You are selecting a channel that doesn't exist in this image."));
-				return;
-			}
-			
-			long offsets=0; 
-			if(channelToFilter>=0) offsets = channelToFilter*pagesz; 
-			
-			const unsigned int Dimension = 3; // \par
-			
-			typedef itk::Image< TInputPixelType, Dimension > InputImageType;
-			typedef itk::Image< TOutputPixelType, Dimension > OutputImageType;
-			typedef itk::ImportImageFilter< TInputPixelType, Dimension > ImportFilterType;
-			
-			typename ImportFilterType::Pointer importFilter = ImportFilterType::New();
-			
-			typename ImportFilterType::SizeType size;
-			size[0] = nx;
-			size[1] = ny;
-			size[2] = nz;
-			
-			typename ImportFilterType::IndexType start;
-			start.Fill( 0 );
-			
-			typename ImportFilterType::RegionType region;
-			region.SetIndex( start );
-			region.SetSize(  size  );
-			
-			importFilter->SetRegion( region );
-			
-			region.SetSize( size );
-			
-			typename InputImageType::PointType origin;
-			origin.Fill( 0.0 );
-			
-			importFilter->SetOrigin( origin );
-			
-			typename ImportFilterType::SpacingType spacing;
-			spacing.Fill( 1.0 );
-			
-			importFilter->SetSpacing( spacing );
-			
-			const bool importImageFilterWillOwnTheBuffer = false;
-			
-			typedef itk::CastImageFilter< InputImageType, OutputImageType> CastImageFilterType;
-			typename CastImageFilterType::Pointer castImageFilter = CastImageFilterType::New();
-			
-			//Generate Speed Image
-			typedef itk::RescaleIntensityImageFilter< OutputImageType, InputImageType >   InvCastFilterType;
 
-			//typedef itk::BinaryThresholdImageFilter< OutputImageType, InputImageType    >    ThresholdingFilterType;
-			//ThresholdingFilterType::Pointer thresholder = ThresholdingFilterType::New();
-			
-			typedef itk::CurvatureAnisotropicDiffusionImageFilter< OutputImageType, OutputImageType >  SmoothingFilterType;
-			typename SmoothingFilterType::Pointer smoothing = SmoothingFilterType::New();
-			
-			//typedef itk::GradientAnisotropicDiffusionImageFilter< OutputImageType, OutputImageType >  SmoothingFilterType;
-			//typename SmoothingFilterType::Pointer smoothing = SmoothingFilterType::New();
-			
-			typedef itk::GradientMagnitudeRecursiveGaussianImageFilter< OutputImageType, OutputImageType >  GradientFilterType;
-			typedef itk::SigmoidImageFilter< OutputImageType, OutputImageType >  SigmoidFilterType;
-			
-			typename GradientFilterType::Pointer  gradientMagnitude = GradientFilterType::New();
-			typename SigmoidFilterType::Pointer sigmoid = SigmoidFilterType::New();
-			
-			sigmoid->SetOutputMinimum(  0.0  );
-			sigmoid->SetOutputMaximum(  1.0  );
-			
-			//FastMarching
-			typedef itk::FastMarchingImageFilter< OutputImageType, OutputImageType > FastMarchingFilterType;
-			typename FastMarchingFilterType::Pointer fastMarching = FastMarchingFilterType::New();
-			
-			typedef typename FastMarchingFilterType::NodeContainer	NodeContainer;
-			typedef typename FastMarchingFilterType::NodeType		NodeType;
-			typename NodeContainer::Pointer seeds = NodeContainer::New();
-			
-			typename OutputImageType::IndexType  seedPosition; // seedPosition[0]  seedPosition[1]  seedPosition[2] 
-			
-			NodeType node;
-			
-			//set \pars
-			const double seedValue = 0.0;
-			
-			LandmarkList list_landmark_sub=callback.getLandmark(win_list[i1]);
-			if(list_landmark_sub.size()<1)
-			{
-				v3d_msg(QObject::tr("You should select one seed from your image."));
-				return;
-			}
-			else
-			{
-			    //seeds
-				seeds->Initialize();
-				
-				for(int i=0;  i<list_landmark_sub.size(); i++)
-				{
-					//
-					seedPosition[0] = list_landmark_sub[i].x -1; // notice 0-based and 1-based difference
-					seedPosition[1] = list_landmark_sub[i].y -1;
-					seedPosition[2] = list_landmark_sub[i].z -1;
-					
-					node.SetValue( seedValue );
-					node.SetIndex( seedPosition );
-					
-					seeds->InsertElement( i, node );
-				}
-			}
-			
-			const double stoppingTime = sqrt(nx*nx + ny*ny + nz*nz);
-			
-			const double sigma = 0.5; // GradientMagnitudeRecursiveGaussianImageFilter
-			
-			const double alpha =  -1; // SigmoidImageFilter
-			const double beta  =  20;
-			
-			//consider multiple channels
-			if(channelToFilter==-1)
-			{
-				TOutputPixelType *output1d;
-				try
-				{
-					output1d = new TOutputPixelType [numberOfPixels];
-				}
-				catch(...)
-				{
-					std::cerr << "Error memroy allocating." << std::endl;
-					return;
-				}
-				
-				const bool filterWillDeleteTheInputBuffer = false;
-				
-				for(long ch=0; ch<nc; ch++)
-				{
-					offsets = ch*pagesz;
-					
-					TOutputPixelType *p = output1d+offsets;
-					
-					importFilter->SetImportPointer( data1d+offsets, pagesz, importImageFilterWillOwnTheBuffer );
-					
-					castImageFilter->SetInput( importFilter->GetOutput() );
-					
-					try
-					{
-						castImageFilter->Update();
-					}
-					catch( itk::ExceptionObject & excp)
-					{
-						std::cerr << "Error run this filter." << std::endl;
-						std::cerr << excp << std::endl;
-						return;
-					}
-					
-					// fast marching
-					// fast marching algorithm
-					smoothing->SetInput( castImageFilter->GetOutput() );
-					gradientMagnitude->SetInput( smoothing->GetOutput() );
-					sigmoid->SetInput( gradientMagnitude->GetOutput() );
-					fastMarching->SetInput( sigmoid->GetOutput() );
-					
-					gradientMagnitude->SetSigma(  sigma  );
-					
-					smoothing->SetTimeStep( 0.125 );
-					smoothing->SetNumberOfIterations(  5 );
-					smoothing->SetConductanceParameter( 9.0 );
-					
-					sigmoid->SetAlpha( alpha );
-					sigmoid->SetBeta(  beta  );
-					
-					fastMarching->SetTrialPoints(  seeds  );
-					fastMarching->SetOutputSize( importFilter->GetOutput()->GetBufferedRegion().GetSize() );
-					fastMarching->SetStoppingValue(  stoppingTime  );
-					
-					//speedimag
-					try
-					{
-						smoothing->Update();
-						gradientMagnitude->Update();
-						sigmoid->Update();
-					}
-					catch( itk::ExceptionObject & excp)
-					{
-						std::cerr << "Error run this filter." << std::endl;
-						std::cerr << excp << std::endl;
-						return;
-					}
-					
-					fastMarching->GetOutput()->GetPixelContainer()->SetImportPointer( p, pagesz, filterWillDeleteTheInputBuffer);
-					
-					try
-					{
-						fastMarching->Update();
-					}
-					catch( itk::ExceptionObject & excp)
-					{
-						std::cerr << "Error run this filter." << std::endl;
-						std::cerr << excp << std::endl;
-						return;
-					}
-					
-				}
-				
-				setPluginOutputAndDisplayUsingGlobalSetting(output1d, nx, ny, nz, nc, callback);
-			}
-			else if(channelToFilter<nc)
-			{
-				importFilter->SetImportPointer( data1d+offsets, pagesz, importImageFilterWillOwnTheBuffer );
-				
-				castImageFilter->SetInput( importFilter->GetOutput() );
-				
-				try
-				{
-					castImageFilter->Update();
-				}
-				catch( itk::ExceptionObject & excp)
-				{
-					std::cerr << "Error run this filter." << std::endl;
-					std::cerr << excp << std::endl;
-					return;
-				}
-				
-				// fast marching algorithm
-				smoothing->SetInput( castImageFilter->GetOutput() );
-				gradientMagnitude->SetInput( smoothing->GetOutput() );
-				sigmoid->SetInput( gradientMagnitude->GetOutput() );
-				fastMarching->SetInput( sigmoid->GetOutput() );
-				
-				gradientMagnitude->SetSigma(  sigma  );
-				
-				smoothing->SetTimeStep( 0.05 );
-				smoothing->SetNumberOfIterations(  5 );
-				smoothing->SetConductanceParameter( 3.0 );
-				
-				sigmoid->SetOutputMinimum(  0.0  );
-				sigmoid->SetOutputMaximum(  1.0  );
-				
-				sigmoid->SetAlpha( alpha );
-				sigmoid->SetBeta(  beta  );
-				
-				fastMarching->SetTrialPoints(  seeds  );
-				fastMarching->SetOutputSize( importFilter->GetOutput()->GetBufferedRegion().GetSize() );
-				fastMarching->SetStoppingValue(  stoppingTime  );
-							
-				//speedimag
-				typename InvCastFilterType::Pointer caster = InvCastFilterType::New();
-				caster->SetInput( sigmoid->GetOutput() );
+  PluginSpecialized( V3DPluginCallback * callback ): Superclass(callback)
+    {
+    this->m_Filter = FilterType::New();
+    this->RegisterInternalFilter( this->m_Filter, 1.0 );
+    }
 
-				try
-				{
-					//smoothing->Update();
-					//gradientMagnitude->Update();
-					//sigmoid->Update();
-					caster->Update();
+  virtual ~PluginSpecialized() {};
 
-				}
-				catch( itk::ExceptionObject & excp)
-				{
-					std::cerr << "Error run this filter." << std::endl;
-					std::cerr << excp << std::endl;
-					return;
-				}
-				
-				typename InputImageType::PixelContainer * container1;
-				
-				container1 =caster->GetOutput()->GetPixelContainer();
-				container1->SetContainerManageMemory( false );
-				
-				typedef TInputPixelType InputPixelType;
-				InputPixelType * output1d_speed = container1->GetImportPointer();
 
-				setPluginOutputAndDisplayUsingGlobalSetting(output1d_speed, nx, ny, nz, 1, callback);
+  void Execute(const QString &menu_name, QWidget *parent)
+    {
+    V3DITKGenericDialog dialog("FastMarching");
 
-				//fastmarching
-				try
-				{
-					fastMarching->Update();
-				}
-				catch( itk::ExceptionObject & excp)
-				{
-					std::cerr << "Error run this filter." << std::endl;
-					std::cerr << excp << std::endl;
-					return;
-				}
+    dialog.AddDialogElement("StoppingTime",50.0, 0.0, 10000.0);
 
-				// output
-				typename OutputImageType::PixelContainer * container;
-				
-				container =fastMarching->GetOutput()->GetPixelContainer();
-				container->SetContainerManageMemory( false );
-				
-				typedef TOutputPixelType OutputPixelType;
-				OutputPixelType * output1d = container->GetImportPointer();
-				
-				setPluginOutputAndDisplayUsingGlobalSetting(output1d, nx, ny, nz, 1, callback);
-			}
+    const double stoppingTime = dialog.GetValue("StoppingTime");
 
-		}
-		
-	}	
-	
+    const double seedValue = 0.0;
+
+    if( dialog.exec() != QDialog::Accepted )
+      {
+      return;
+      }
+
+    this->m_Filter->SetStoppingValue( stoppingTime );
+
+    typedef typename FilterType::NodeContainer  NodeContainer;
+    typedef typename FilterType::NodeType       NodeType;
+
+    typename NodeContainer::Pointer seedPoints = NodeContainer::New();
+
+    NodeType node;
+
+    seedPoints->Initialize();
+
+    v3dhandleList windowList = this->m_V3DPluginCallback->getImageWindowList();
+
+    LandmarkList listOfLandmarks = this->m_V3DPluginCallback->getLandmark( windowList[0] ); // FIXME
+
+    unsigned int numberOfSeedPoints = listOfLandmarks.size();
+
+    if ( ! numberOfSeedPoints )
+      {
+      v3d_msg(QObject::tr("You should select one seed from your image."));
+      return;
+      }
+
+    typedef itk::Image< TPixelType, 3 > OutputImageType; // FIXME
+    typename OutputImageType::IndexType  seedPosition;
+
+    for(unsigned int i = 0;  i < numberOfSeedPoints; i++ )
+      {
+      seedPosition[0] = listOfLandmarks[i].x - 1; // notice 0-based and 1-based difference
+      seedPosition[1] = listOfLandmarks[i].y - 1;
+      seedPosition[2] = listOfLandmarks[i].z - 1;
+
+      node.SetValue( seedValue );
+      node.SetIndex( seedPosition );
+
+      seedPoints->InsertElement( i, node );
+      }
+
+    this->m_Filter->SetTrialPoints( seedPoints );
+
+    this->Compute();
+    }
+
+  virtual void ComputeOneRegion()
+    {
+
+    this->m_Filter->SetInput( this->GetInput3DImage() );
+
+    this->m_Filter->Update();
+
+    this->SetOutputImage( this->m_Filter->GetOutput() );
+    }
+
+
+private:
+
+    typename FilterType::Pointer   m_Filter;
+
 };
 
-#define EXECUTE( v3d_pixel_type, input_pixel_type, output_pixel_type ) \
-	case v3d_pixel_type: \
-	{ \
-		ITKFastMarchingSpecializaed< input_pixel_type, output_pixel_type > runner; \
-		runner.Execute( callback, parent ); \
-		break; \
-	} 
 
-#define EXECUTE_ALL_PIXEL_TYPES \
-	if (! p4DImage) return; \
-	ImagePixelType pixelType = p4DImage->getDatatype(); \
-	switch( pixelType )  \
-	{  \
-		EXECUTE( V3D_UINT8, unsigned char, float );  \
-		EXECUTE( V3D_UINT16, unsigned short int, float );  \
-		EXECUTE( V3D_FLOAT32, float, float );  \
-		case V3D_UNKNOWN:  \
-		{  \
-		}  \
-	}  
-
-
-void itkFastMarchingPlugin(V3DPluginCallback &callback, QWidget *parent)
-{
-	Image4DSimple* p4DImage = callback.getImage(callback.currentImageWindow());
-	if (!p4DImage)
-    {
-		v3d_msg(QObject::tr("You don't have any image open in the main window."));
-		return;
+#define EXECUTE_PLUGIN_FOR_ONE_IMAGE_TYPE( v3d_pixel_type, c_pixel_type ) \
+  case v3d_pixel_type: \
+    { \
+    PluginSpecialized< c_pixel_type > runner( &callback ); \
+    runner.Execute( menu_name, parent ); \
+    break; \
     }
-	
-	EXECUTE_ALL_PIXEL_TYPES;
+
+
+void FastMarchingPlugin::dofunc(const QString & func_name,
+    const V3DPluginArgList & input, V3DPluginArgList & output, QWidget * parent)
+{
+  // empty by now
 }
 
+
+void FastMarchingPlugin::domenu(const QString & menu_name, V3DPluginCallback & callback, QWidget * parent)
+{
+  if (menu_name == QObject::tr("about this plugin"))
+    {
+    QMessageBox::information(parent, "Version info", "ITK FastMarching 1.0 (2010-Jun-21): this plugin is developed by Luis Ibanez.");
+    return;
+    }
+
+  v3dhandle curwin = callback.currentImageWindow();
+  if (!curwin)
+    {
+    v3d_msg(tr("You don't have any image open in the main window."));
+    return;
+    }
+
+  Image4DSimple *p4DImage = callback.getImage(curwin);
+  if (! p4DImage)
+    {
+    v3d_msg(tr("The input image is null."));
+    return;
+    }
+
+  EXECUTE_PLUGIN_FOR_ALL_PIXEL_TYPES;
+}
 
--- Source/LevelSets/ITKFastMarching.h
+++ Source/LevelSets/ITKFastMarching.h
@@ -1,99 +1,13 @@
-/* ITKFastMarching.h
- * 2010-06-02: create this program by Yang Yu
- */
+#ifndef __FastMarching_H__
+#define __FastMarching_H__
 
-#ifndef __ITKFASTMARCHING_H__
-#define __ITKFASTMARCHING_H__
+#include "V3DITKPluginDefaultHeader.h"
 
-//   FAST MARCHING.
-//
-
-#include <QtGui>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <v3d_interface.h>
-
-class ITKFastMarchingPlugin : public QObject, public V3DPluginInterface
+class FastMarchingPlugin : public QObject, public V3DPluginInterface
 {
-    Q_OBJECT
-    Q_INTERFACES(V3DPluginInterface);
-	
-public:
-	QStringList menulist() const;
-	void domenu(const QString &menu_name, V3DPluginCallback &callback, QWidget *parent);
-	
-	QStringList funclist() const {return QStringList();}
-	void dofunc(const QString &func_name, const V3DPluginArgList &input, V3DPluginArgList &output, QWidget *parent) {}
-	
+  Q_OBJECT
+  Q_INTERFACES(V3DPluginInterface)
+  V3DITKPLUGIN_DEFAULT_CLASS_DECLARATION_BODY(FastMarching);
 };
-
-class ITKFastMarchingDialog : public QDialog
-{
-	Q_OBJECT
-	
-public:
-	ITKFastMarchingDialog(V3DPluginCallback &callback, QWidget *parent)
-	{
-		v3dhandleList win_list = callback.getImageWindowList();
-		
-		QStringList items;
-		for (int i=0; i<win_list.size(); i++) items << callback.getImageName(win_list[i]);
-		
-		// image
-		label_subject = new QLabel(QObject::tr("Choose An Image (with one marker): ")); 
-		combo_subject =  new QComboBox(); combo_subject->addItems(items);
-		
-		// when no image open return
-		if(win_list.size()<1) 
-		{
-			QMessageBox::information(0, "Fast Marching", QObject::tr("No image is open."));
-			return;
-		}
-		
-		printf("Passing data to data1d\n");
-		
-		ok     = new QPushButton("OK");
-		cancel = new QPushButton("Cancel");
-		
-		//gridlayout
-		gridLayout = new QGridLayout();
-		
-		gridLayout->addWidget(label_subject, 0,0,1,1); gridLayout->addWidget(combo_subject, 0,1,1,4);
-		gridLayout->addWidget(cancel, 2,4); gridLayout->addWidget(ok, 2,5);
-		setLayout(gridLayout);
-		setWindowTitle(QString("Fast Marching"));
-		
-		//slot
-		connect(ok,     SIGNAL(clicked()), this, SLOT(accept()));
-		connect(cancel, SIGNAL(clicked()), this, SLOT(reject()));
-		
-		connect(combo_subject, SIGNAL(currentIndexChanged(int)), this, SLOT(update()));
-	}
-	
-	~ITKFastMarchingDialog(){}
-	
-public slots:
-	void update()
-	{
-		i1 = combo_subject->currentIndex();
-	}
-	
-	
-public:
-	
-	int i1,i2;
-	
-	QGridLayout *gridLayout;
-	
-	QLabel* label_subject;
-	QComboBox* combo_subject;
-	
-	
-	QPushButton* ok;
-	QPushButton* cancel;
-};
-
 
 #endif
